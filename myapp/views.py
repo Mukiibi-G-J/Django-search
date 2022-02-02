359a5276af93a94887e6e81b85973200e450dc43
Mukiibi-J-G <mukiibijosephgilbert865@gmail.com>
2022-02-02 18:12:36 +0300

    modified

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'myapp/home.html')
+
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    stuff_frontend = {
+        'search': search
+    }
+    return render(request, 'myapp/new_search.html', stuff_frontend)
